72790c841ef44861f83efffd0a5c7e7fd2ac0187
Dmirtii <163983392+ZhdanoffD@users.noreply.github.com>
2024-12-17 13:14:14 +0100

    Added  source code and start settings

---
+++ Source/GasDungeon/Public/GasDungeonAssetManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/AssetManager.h"
+#include "GasDungeonAssetManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GASDUNGEON_API UGasDungeonAssetManager : public UAssetManager
+{
+	GENERATED_BODY()
+	
+};
